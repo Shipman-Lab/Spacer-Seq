f4adc0d7ad98eacc4b8523de85d81023ec74cf26
Santi Bhattarai-Kline <santi.bhattaraikline@gladstone.ucsf.edu>
2019-11-12 14:27:14 -0800

    let's get this repo started

--- BatchRun_Fed_SPCRs_v2.cmd
+++
@@ -1,7 +0,0 @@
-cd Dropbox (Gladstone)/eVOLVER_and_Retro_Record/MiSeq_Data/
-for /L %%i in (1,1,9) do python Fed_SPCRs_v2.py msSBK_2 msSBK_2_0%%i_trimmed local
-for /L %%i in (10,1,42) do python Fed_SPCRs_v2.py msSBK_2 msSBK_2_%%i_trimmed local
-
-cmd /k
-
-REF python Fed_SPCRs_v2.py msSBK_1 msSBK_1_56_trimmed local
--- BatchRun_SPCR_blast_v2.bat
+++
@@ -1,9 +0,0 @@
-cd /
-
-for %%i in (01, 02, 03, 04, 05, 06, 07, 08, 09) do (
-python "C:\Users\santi.bhattaraikline\Dropbox (Gladstone)\eVOLVER_and_Retro_Record/MiSeq_Data/SPCR_blast_v2.py" 2 %%i local BL21_DUETb3v2msdr_Cas_mphR86RT)
-
-for /L %%i in (10,1,42) do (
-python "C:\Users\santi.bhattaraikline\Dropbox (Gladstone)\eVOLVER_and_Retro_Record/MiSeq_Data/SPCR_blast_v2.py" 2 %%i local BL21_DUETb3v2msdr_Cas_mphR86RT)
-
-cmd /k
