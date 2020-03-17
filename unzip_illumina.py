ee57d8b8d3a7bb68c152990c96eb55701d876d4f
Santi Bhattarai-Kline <santi.bhattaraikline@gladstone.ucsf.edu>
2020-03-17 15:32:42 -0700

    add stop/checkpoit in unzip utility

--- unzip_illumina.py
+++ unzip_illumina.py
@@ -6,33 +6,43 @@ user_profile = os.environ ['USERPROFILE']
 File_Path = input("File Path (MiSeq Run Folder):")
 runName = input("Run Name (No Spaces):")
 Data_Path = '%s/%s_rawIndexedFASTQs' % (File_Path, runName)
-
-# Create Data folder
 newpath = ((r'%s') % (Data_Path))
-if not os.path.exists(newpath): os.makedirs(newpath)
 
-interior_folder = os.listdir(File_Path)[0]
+def unzip():
+# Sort through MiSeq run output folder and unzip each file inside.
+# Unzipped files written to global "Data_Path"
 
-folder_list = os.listdir('%s/%s' % (File_Path,interior_folder))
-for folder in folder_list:
-	junkID = folder.split('_')[-1]
-	sampleID = folder[:len(folder) - len(junkID) - 1]
-	print('Unzipping ' + sampleID)
-	file_name = os.listdir('%s/%s/%s' % (File_Path,interior_folder,folder))[0]
-	with gzip.open('%s/%s/%s/%s' % (File_Path,interior_folder,folder,file_name), 'rb') as f_in:
-		with open('%s/%s.fastq' % (Data_Path,sampleID), 'wb') as f_out:
-			shutil.copyfileobj(f_in, f_out)
+	interior_folder = os.listdir(File_Path)[0]
 
-
-
-
-
+	folder_list = os.listdir('%s/%s' % (File_Path,interior_folder))
+	for folder in folder_list:
+		junkID = folder.split('_')[-1]
+		sampleID = folder[:len(folder) - len(junkID) - 1]
+		print('Unzipping ' + sampleID)
+		file_name = os.listdir('%s/%s/%s' % (File_Path,interior_folder,folder))[0]
+		with gzip.open('%s/%s/%s/%s' % (File_Path,interior_folder,folder,file_name), 'rb') as f_in:
+			with open('%s/%s.fastq' % (Data_Path,sampleID), 'wb') as f_out:
+				shutil.copyfileobj(f_in, f_out)
 
 
+if not os.path.exists(newpath):
+	os.makedirs(newpath)
+	unzip()
+else:
+	print("You have already unzipped these files.")
+	response = input("Do you wish to repeat? (y = yes, n = no) -- ")
+	if response == "y":
+		unzip()
 
+def unzip():
+	interior_folder = os.listdir(File_Path)[0]
 
-
-
-# with gzip.open('file.txt.gz', 'rb') as f_in:
-#     with open('file.txt', 'wb') as f_out:
-#         shutil.copyfileobj(f_in, f_out)
+	folder_list = os.listdir('%s/%s' % (File_Path,interior_folder))
+	for folder in folder_list:
+		junkID = folder.split('_')[-1]
+		sampleID = folder[:len(folder) - len(junkID) - 1]
+		print('Unzipping ' + sampleID)
+		file_name = os.listdir('%s/%s/%s' % (File_Path,interior_folder,folder))[0]
+		with gzip.open('%s/%s/%s/%s' % (File_Path,interior_folder,folder,file_name), 'rb') as f_in:
+			with open('%s/%s.fastq' % (Data_Path,sampleID), 'wb') as f_out:
+				shutil.copyfileobj(f_in, f_out)
