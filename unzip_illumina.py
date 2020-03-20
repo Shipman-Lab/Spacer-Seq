c34ce472a356d945cad9383170d8f08ad91c9819
Santi Bhattarai-Kline <santi.bhattaraikline@gladstone.ucsf.edu>
2020-03-20 12:34:26 -0700

    change folder selection to FASTQ_generation

--- unzip_illumina.py
+++ unzip_illumina.py
@@ -3,23 +3,23 @@ import shutil
 import os, sys
 
 user_profile = os.environ ['USERPROFILE']
-File_Path = input("File Path (MiSeq Run Folder):")
-Data_Path = '%s/rawIndexedFASTQs' % (File_Path, runName)
+File_Path = input("File Path (MiSeq Run \"FASTQ Generation\" Folder):")
+parentFolder = os.path.split(File_Path)[0]
+Data_Path = '%s/rawIndexedFASTQs' % (parentFolder)
 newpath = ((r'%s') % (Data_Path))
 
 def unzip():
 # Sort through MiSeq run output folder and unzip each file inside.
 # Unzipped files written to global "Data_Path"
 
-	interior_folder = os.listdir(File_Path)[0]
 
-	folder_list = os.listdir('%s/%s' % (File_Path,interior_folder))
+	folder_list = os.listdir('%s' % (File_Path))
 	for folder in folder_list:
 		junkID = folder.split('_')[-1]
 		sampleID = folder[:len(folder) - len(junkID) - 1]
 		print('Unzipping ' + sampleID)
-		file_name = os.listdir('%s/%s/%s' % (File_Path,interior_folder,folder))[0]
-		with gzip.open('%s/%s/%s/%s' % (File_Path,interior_folder,folder,file_name), 'rb') as f_in:
+		file_name = os.listdir('%s/%s' % (File_Path,folder))[0]
+		with gzip.open('%s/%s/%s' % (File_Path,folder,file_name), 'rb') as f_in:
 			with open('%s/%s.fastq' % (Data_Path,sampleID), 'wb') as f_out:
 				shutil.copyfileobj(f_in, f_out)
 
